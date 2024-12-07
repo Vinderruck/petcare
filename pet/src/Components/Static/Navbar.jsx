1b386f6f506f87671835516bbaf91aaf0138f5f0
Vinderruck <majevincent0@gmail.com>
2024-12-07 10:03:35 +0300

    about

--- pet/src/Components/Static/Navbar.jsx
+++ pet/src/Components/Static/Navbar.jsx
@@ -88,15 +88,19 @@ const [open, setopen] = useState(false)
     <p className="text-[1.2rem] montserrat">
     At Tazama Rafiki, we understand that your pets are more than just animals — they're family.
      When you’re away on holiday, <br/>the last thing you want is to worry about your furry, feathered, 
-     or scaly friends. Traditional kennels can often be stressful for pets, <br/>sleaving both you and them feeling uneasy. 
+     or scaly friends. Traditional kennels can often be stressful for pets, <br/>
+     <div style={{background:"black",width:"100%",height:"1px",justifyContent:"center", display:"flex",alignItems:"center", margin:"6px"}}/>sleaving both you and them feeling uneasy. 
      That's why we offer a solution that lets your 
     pets stay in the comfort of their own home <br/>while receiving the care they need.
 
 Whether you're going away for a few days or need longer-term care, Tazama 
-Rafiki provides personalized, <br/>home-based pet care services to ensure your
+Rafiki provides personalized, 
+<div style={{background:"black",width:"100%",height:"1px",justifyContent:"center", display:"flex",alignItems:"center", margin:"6px"}}/>
+<br/>home-based pet care services to ensure your
  pets are well-fed, walked, and attended to, just as you would. We understand 
  that sometimes<br/> it can be difficult to arrange for staff, especially when everyone 
- is heading off for their own holidays, but we’re here to make sure your pet’s<br/> routine stays intact.
+ is heading off for their own holidays, but we’re here to make sure your pet’s
+ <br/><div style={{background:"black",width:"100%",height:"1px",justifyContent:"center", display:"flex",alignItems:"center", margin:"6px"}}/> routine stays intact.
 
 With Tazama Rafiki, you can rest easy knowing your pets will be in loving, 
 responsible hands. Let us take care of the <br/>details so you can enjoy your holiday without any worries!
