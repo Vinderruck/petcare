1783f315ba473e76ca56ae4c80b49c9094595c90
Vinderruck <majevincent0@gmail.com>
2024-12-07 09:19:22 +0300

    about

---
+++ pet/src/Components/Static/AboutUs.jsx
@@ -0,0 +1,10 @@
+ import React from 'react';
+ import Wrapper from '../HOC/Wrapper';
+ 
+ const AboutUs = () => {
+   return (
+     <div>AboutUs</div>
+   )
+ }
+ 
+ export default Wrapper (AboutUs,"about")
--- pet/src/Components/Static/Navbar.jsx
+++ pet/src/Components/Static/Navbar.jsx
@@ -43,7 +43,7 @@ const [open, setopen] = useState(false)
         <div className="logdiv" style={{marginTop:"2px"}}><img src={logo2} alt="logo" className="logo"/></div>
     <div className="NavbarRight">
       <Link to="#home" className="navLinks">Home</Link>
-    <Link to="#" className="navLinks">About</Link>
+    <Link to="#about" className="navLinks">About</Link>
     <Link to="#" className="navLinks">Service offered</Link>
     <Link to="#" className="navLinks">Booking </Link>
     </div>
@@ -52,7 +52,7 @@ const [open, setopen] = useState(false)
     {open &&(
       <div className="navsmright bg-white" >
          <Link to="#home" className="navLinks">Home</Link>
-    <Link to="#" className="navLinks">About</Link>
+    <Link to="#about" className="navLinks">About</Link>
     <Link to="#" className="navLinks">Service offered</Link>
     <Link to="#" className="navLinks">Booking </Link>
       </div>
@@ -81,6 +81,28 @@ const [open, setopen] = useState(false)
     </div> 
     </div> 
     <div style={{background:"white",width:"100%",height:"1px",justifyContent:"center", display:"flex",alignItems:"center", margin:"6px"}}/>
+  
+  <div id="about"> 
+    <h1 style={{justifyContent:"center",alignItems:"center",textAlign:"center",margin:"3px",color:"white", fontSize:"1.5rem"}}>About Us</h1>
+    <div className="gap-4 grid grid-cols-3 bg-slate-50 opacity-80 footer">
+    <p className="text-[1.2rem] montserrat">
+    At Tazama Rafiki, we understand that your pets are more than just animals — they're family.
+     When you’re away on holiday, <br/>the last thing you want is to worry about your furry, feathered, 
+     or scaly friends. Traditional kennels can often be stressful for pets, <br/>sleaving both you and them feeling uneasy. 
+     That's why we offer a solution that lets your 
+    pets stay in the comfort of their own home <br/>while receiving the care they need.
+
+Whether you're going away for a few days or need longer-term care, Tazama 
+Rafiki provides personalized, <br/>home-based pet care services to ensure your
+ pets are well-fed, walked, and attended to, just as you would. We understand 
+ that sometimes<br/> it can be difficult to arrange for staff, especially when everyone 
+ is heading off for their own holidays, but we’re here to make sure your pet’s<br/> routine stays intact.
+
+With Tazama Rafiki, you can rest easy knowing your pets will be in loving, 
+responsible hands. Let us take care of the <br/>details so you can enjoy your holiday without any worries!
+    </p></div>
+  </div>
+  
    <div className="gap-4 grid grid-cols-3 bg-slate-50 opacity-80 footer">
     <div className="footerdivs">
       <h2 className="text-[1.5rem] lobster-two-bold">Need help?</h2>
