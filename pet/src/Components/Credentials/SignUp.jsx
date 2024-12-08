4843de296806931d926c4c476da63311ef336ca8
Vinderruck <majevincent0@gmail.com>
2024-12-08 16:27:31 +0300

    100% width input

--- pet/src/Components/Credentials/SignUp.jsx
+++ pet/src/Components/Credentials/SignUp.jsx
@@ -10,22 +10,22 @@ const SignUp = () => {
              <div className="logsign" style={{marginTop:"2px"}}><img src={logo2} alt="logo" className="logo1"/></div>
         <Form className="flex flex-col justify-center items-center bg-lime-500 w-[80%] overflow-auto formSign">
    
-        <Form.Group className="flex flex-col gap-1 m-2 w-[80%]">
+        <Form.Group className="flex flex-col gap-1 m-2 w-[100%] sm:w-[80%]">
             <Form.Label className="text-[1rem] montserrat"> Email</Form.Label>
 
             <Form.Control placeholder="Enter Your Emal" className="justify-center w-full hover:cursor-pointer"/>
         </Form.Group>
-        <Form.Group className="flex flex-col gap-1 m-2 w-[80%]">
+        <Form.Group className="flex flex-col gap-1 m-2 w-[100%] sm:w-[80%]">
             <Form.Label className="text-[1rem] montserrat">Location</Form.Label>
 
             <Form.Control placeholder="Enter Your Location" className="justify-center w-full hover:cursor-pointer"/>
         </Form.Group>
-        <Form.Group className="flex flex-col gap-1 m-2 w-[80%]">
+        <Form.Group className="flex flex-col gap-1 m-2 w-[100%] sm:w-[80%]">
             <Form.Label className="text-[1rem] montserrat"> Create Password</Form.Label>
 
             <Form.Control  type="password" placeholder="Create Password" className="justify-center w-full hover:cursor-pointer"/>
         </Form.Group>
-        <Form.Group className="flex flex-col gap-1 m-2 w-[80%]">
+        <Form.Group className="flex flex-col gap-1 m-2 w-[100%] sm:w-[80%]">
             <Form.Label className="text-[1rem] montserrat"> Confirm Password</Form.Label>
 
             <Form.Control  type="password" placeholder="Confirm Your Password" className="justify-center w-full hover:cursor-pointer"/>
